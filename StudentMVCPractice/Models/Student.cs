e3f7c1c69574c80abece4a79c366516a682c1f33
Kendall Dawson <lilkenny642@gmail.com>
2025-08-01 20:39:10 -0700

    Add Student class with properties and documentation
    Introduced a new `Student` class in the `StudentMVCPractice.Models` namespace. The class includes properties for `StudentId`, `FirstName`, `LastName`, `PreferredName`, and `DateOfBirth`, with appropriate required fields. XML documentation comments have been added for clarity on each property.

---
+++ StudentMVCPractice/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿namespace StudentMVCPractice.Models;
+
+/// <summary>
+/// Reperesents a student at a school.
+/// </summary>
+public class Student
+{
+    /// <summary>
+    /// A unique identifier for the student (primary key).
+    /// </summary>
+    public int StudentId { get; set; }
+
+    /// <summary>
+    /// The first name of the student.
+    /// </summary>
+    public required string FirstName { get; set; }
+
+    /// <summary>
+    /// The last name of the student.
+    /// </summary>
+    public required string LastName { get; set; }
+
+    /// <summary>
+    /// The name the student prefers to be called. This is
+    /// optional to fill out and can be left empty.
+    /// </summary>
+    public string PreferredName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The student's date of birth.
+    /// </summary>
+    public required DateOnly DateOfBirth { get; set; }
+
+
+}
