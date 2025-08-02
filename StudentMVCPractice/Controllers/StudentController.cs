29b985cd69c491ef4a8eeffb9c526f9ebb7e4b02
Kendall Dawson <lilkenny642@gmail.com>
2025-08-01 20:59:33 -0700

    Add StudentController with Index and Create actions
    This commit introduces a new `StudentController` class in the
    `StudentMVCPractice.Controllers` namespace. The controller
    inherits from `Controller` and includes two action methods:
    `Index` and `Create`, both returning views. This addition
    supports the management of student-related views in the
    ASP.NET MVC application.

---
+++ StudentMVCPractice/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentMVCPractice.Controllers;
+public class StudentController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
